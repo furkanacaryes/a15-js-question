13fd822739d3188470288d8d99e5775fd8764bd6
Furkan Acar <furkan@acar.digital>
2019-09-05 01:16:11 +0300

    Vowel harmony function implemented.

--- major_vowel_harmony.js
+++ major_vowel_harmony.js
@@ -1,5 +1,23 @@
-const isHasMajorVowelHarmony = (word) => {
-  console.log('not implemented');
+
+const vowels = {
+  hard: ['a', 'ı', 'o', 'u'],
+  soft: ['e', 'i', 'ö', 'ü']
+};
+
+const hasVowelWithType = (word, vowelType) => (
+  word.split('')
+      .some(char => vowels[vowelType]
+        .some(vowel => vowel === char))
+);
+
+const hasMajorVowelHarmony = (word) => {
+  const firstSyllable = word.slice(0, 2);
+
+  if (hasVowelWithType(firstSyllable, 'hard')) {
+    return !hasVowelWithType(word, 'soft');
+  } else {
+    return hasVowelWithType(word, 'soft');
+  }
 }
 
-export default isHasMajorVowelHarmony;
+export default hasMajorVowelHarmony;
